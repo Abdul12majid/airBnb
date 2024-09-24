14a9119831144817b8ec5ef39d77da469f4df335
Abdul12majid <yisaabdulmajid@gmail.com>
2024-09-24 16:03:05 +0100

    login func

--- users/views.py
+++ users/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render, HttpResponse
 from django.contrib.auth import login, logout, authenticate
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import LoginSerializer
+from django.contrib.auth.models import User
 
 # Create your views here.
+
+@api_view(['POST', "GET"])
 def login_user(request):
-	return HttpResponse("Login view")
+	serializer = LoginSerializer(data=request.data)
+	if serializer.is_valid():
+		username = request.data['username']
+		password = request.data['password']
+		user = authenticate(request, username=username, password=password)
+		if user is not None:
+			login(request, user)
+			return Response({'Info': 'Login Successful'})
+		else:
+			return Response({'Info': 'Incorrect username or password'})
+
+	return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
