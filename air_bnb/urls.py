8eccdad570f368ab58b5d3d2c650a3925889d03d
Abdul12majid <yisaabdulmajid@gmail.com>
2024-09-24 16:06:43 +0100

    fixed login func

--- air_bnb/urls.py
+++ air_bnb/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include('bnb_app.urls')),
-    path("login", include('users.urls')),
+    path("users/", include('users.urls')),
 ]
--- users/serializers.py
+++ users/serializers.py
@@ -4,4 +4,4 @@ from django.contrib.auth.models import User
 
 class LoginSerializer(serializers.Serializer):
 	username = serializers.CharField(max_length=500)
-    password = serializers.CharField(max_length=500, write_only=True)
+	password = serializers.CharField(max_length=500, write_only=True)
