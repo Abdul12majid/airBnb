bd234fd3da1f2167af5b6f43f3474cf88486719b
Abdul12majid <yisaabdulmajid@gmail.com>
2024-09-24 15:47:24 +0100

    users app set-up

--- air_bnb/urls.py
+++ air_bnb/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include('bnb_app.urls')),
+    path("login", include('users.urls')),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from django.contrib.auth import login, logout, authenticate
 
 # Create your views here.
+def login_user(request):
+	return HttpResponse("Login view")
