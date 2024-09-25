49c623bb3812ddfc3be0b2c4872f22e48f6656b0
Abdul12majid <yisaabdulmajid@gmail.com>
2024-09-25 16:11:19 +0100

    sign-up view

--- users/views.py
+++ users/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth import login, logout, authenticate
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import LoginSerializer
+from .serializers import LoginSerializer, SignUpSerializer
 from django.contrib.auth.models import User
 
 # Create your views here.
@@ -21,4 +21,17 @@ def login_user(request):
 		else:
 			return Response({'Info': 'Incorrect username or password'})
 
+	return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST', 'GET'])	
+def register(request):
+	serializer = SignUpSerializer(data=request.data)
+	if serializer.is_valid():
+		serializer.save()
+		response = {
+			"info":"Registeration successful",
+			"data":serializer.data
+		}
+		return Response(data=response, status=status.HTTP_201_CREATED)
 	return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
