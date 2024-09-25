3de1ba58f49974a77b2f939938b0605aa87d66de
Abdul12majid <yisaabdulmajid@gmail.com>
2024-09-25 16:40:09 +0100

    create model Listing

--- bnb_app/models.py
+++ bnb_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Listing(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    location = models.CharField(max_length=255)
+    price_per_night = models.DecimalField(max_digits=10, decimal_places=2)
+    amenities = models.TextField()
+    host = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_available = models.BooleanField(default=True)
