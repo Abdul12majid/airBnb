38dec156f7014f8f4fe30c6f7e10c424ba588507
Abdul12majid <yisaabdulmajid@gmail.com>
2024-09-25 16:48:00 +0100

    create mode Review

--- bnb_app/models.py
+++ bnb_app/models.py
@@ -27,3 +27,13 @@ class Booking(models.Model):
     check_out_date = models.DateField()
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
     status = models.ForeignKey(Booking_status, on_delete=models.CASCADE)
+
+
+class Review(models.Model):
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    guest = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField(choices=[(1, '1 star'), (2, '2 stars'), (3, '3 stars'), (4, '4 stars'), (5, '5 stars')])
+    comment = models.TextField()
+
+    def __init__(self):
+		return (f'{self.guest.username} review')
