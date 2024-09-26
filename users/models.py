79b9f6b27a6a18e97f5b1877507b0a7563f25ba7
Abdul12majid <yisaabdulmajid@gmail.com>
2024-09-26 07:42:39 +0100

    created profile model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    user=models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_no = models.CharField(max_length=255)
+    date_of_birth=models.DateTimeField(auto_now=True)
+    profile_image = models.URLField(null=True, blank=True)
+    profile_bio=models.CharField(null=True, blank=True, max_length=500)
+
+    def __str__(self):
+        return self.user.username
+
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        user_profile=Profile(user=instance)
+        user_profile.save()
+post_save.connect(create_profile, sender=User)
