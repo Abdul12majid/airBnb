5ca6e688cbdb3a7fc4ef02f5d6b1735ec8603074
Abdul12majid <yisaabdulmajid@gmail.com>
2024-09-28 15:15:41 +0100

    changed review view

--- bnb_app/urls.py
+++ bnb_app/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path("", views.index),
     path("properties", views.properties.as_view()),
     path("property/<int:pk>", views.property_info.as_view()),
-    path("reviews", views.reviews.as_view()),
+    path("reviews", views.reviews),
     path("bookings", views.bookings.as_view()),
     path("book/<int:pk>", views.book)
 
--- bnb_app/views.py
+++ bnb_app/views.py
@@ -35,10 +35,31 @@ class property_info(RetrieveUpdateDestroyAPIView):
 	serializer_class = PropSerializer
 
 
-class reviews(ListCreateAPIView):
-	queryset = Review.objects.all()
-	pagination_class = PageNumberPagination
-	serializer_class = RevSerializer
+@api_view(['POST', 'GET'])
+def reviews(request):
+	all_reviews = Review.objects.all()
+	serializer_class = RevSerializer(all_reviews, many=True)
+	new_review = RevSerializer(data=request.data)
+	if new_review.is_valid():
+		prop_id = request.data['listing']
+		listing = Listing.objects.get(id=prop_id)
+		rate_id = request.data['rating']
+		rating = Rating.objects.get(id=rate_id)
+		guest = request.user
+		comment = request.data['comment']
+		user_review = Review.objects.create(
+			listing=listing,
+			rating=rating,
+			guest=guest,
+			comment=comment
+			)
+		user_review.save()
+
+		return Response({'Message':"Thanks for your feedback", 'info':new_review.data})
+	else:
+		return Response({'info':serializer_class.data})
+
+
 
 
 class bookings(ListCreateAPIView):
@@ -74,5 +95,6 @@ def book(request, pk):
 
 
 
+#list and tuples and dictionaries manipulations
 
 
