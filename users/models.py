98ee9cb95eefd939cb5099f2e872d857d8306212
Abdul12majid <yisaabdulmajid@gmail.com>
2024-10-03 15:53:53 +0100

    created userbooking model

--- users/models.py
+++ users/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
-from bnb_app.models import Listing
+from bnb_app.models import Listing, Booking_status
+
 
 # Create your models here.
 class Profile(models.Model):
@@ -22,3 +23,14 @@ def create_profile(sender, instance, created, **kwargs):
         user_profile.save()
 post_save.connect(create_profile, sender=User)
 
+
+class UserBooking(models.Model):
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    guest = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_bookings')
+    check_in_date = models.DateField()
+    check_out_date = models.DateField()
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.ForeignKey(Booking_status, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return (f'{self.guest.username} booked {self.listing.title}')
